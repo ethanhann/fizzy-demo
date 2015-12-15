94442fb47820ac9092b68d20dca1bd36e1ceeb72
Ethan Hann <ethanhann@gmail.com>
2015-12-14 23:32:05 -0500

    Put together a little front-end app

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,17 @@
 'use strict';
 
-// ...
-
-
+(function (axios, Vue) {
+    axios.get('/api/PersonListRequest')
+    .then(function (response) {
+        console.log(response);
+        new Vue({
+            el: '#app',
+            data: {
+                people: response.data
+            }
+        });
+    })
+    .catch(function (response) {
+        console.log(response);
+    });
+})(axios, Vue);
